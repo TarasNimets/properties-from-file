a931203f60d8dcd81b9e8572fcbf4053693645d9
tarasnimets <tar.nimets@gmail.com>
2018-03-31 18:05:49 +0300

    Initial Commit

---
+++ src/main/java/com/tequilas/test/service/Property.java
@@ -0,0 +1,61 @@
+package com.tequilas.test.service;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+public final class Property {
+
+    private static String name;
+    private static int age;
+    private static long code;
+    private static boolean status;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            FileInputStream fInputStream = new FileInputStream("prop.properties");
+            properties.load(fInputStream);
+            fInputStream.close();
+
+            String fieldName = "";
+            String fieldType = "";
+            for (Field field : Property.class.getDeclaredFields()) {
+                fieldName = field.getName();
+                fieldType = field.getType().getSimpleName();
+
+                if (fieldType.equals("String")) {
+                    field.set(null, properties.getProperty(fieldName));
+                } else if (fieldType.equals("int")) {
+                    field.set(null, Integer.parseInt(properties.getProperty(fieldName)));
+                } else if (fieldType.equals("long")) {
+                    field.set(null, Long.parseLong(properties.getProperty(fieldName)));
+                } else if (fieldType.equals("boolean")) {
+                    field.set(null, Boolean.parseBoolean(properties.getProperty(fieldName)));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Property() {
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static long getCode() {
+        return code;
+    }
+
+    public static boolean isStatus() {
+        return status;
+    }
+
+}
---
+++ src/main/java/com/tequilas/test/view/App.java
@@ -0,0 +1,14 @@
+package com.tequilas.test.view;
+
+import com.tequilas.test.service.Property;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println(Property.getName());
+        System.out.println(Property.getAge());
+        System.out.println(Property.getCode());
+        System.out.println(Property.isStatus());
+    }
+
+}
